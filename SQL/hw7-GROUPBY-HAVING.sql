2db647a452b32a1e26d263cf85aa6169234fff41
Büşra Can <48771071+busracncn@users.noreply.github.com>
2022-05-31 10:43:21 +0300

    Create hw7-GROUPBY-HAVING.sql

---
+++ SQL/hw7-GROUPBY-HAVING.sql
@@ -0,0 +1,35 @@
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+
+SELECT rating FROM film
+GROUP BY rating;
+
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+
+--ex1
+SELECT country_id, COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1; --44 60
+
+--ex2 Sadece en fazla şehir sayısını veriyor, şehir bilgisini eklenmek istediğinde hata alıyor
+SELECT MAX(count)
+FROM (SELECT country_id, COUNT(*) count FROM city GROUP BY country_id) AS sq GROUP BY country_id; --60
+
+--ex3
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+HAVING COUNT(*) = (SELECT MAX(count) FROM (SELECT country_id, COUNT(*) count FROM city GROUP BY country_id) AS sq) --44 60
+    
+    
+ 
